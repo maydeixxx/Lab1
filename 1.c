ec644895dff9071ec08e4e5b840b3d8c3c0260b8
Alex Amaev <139319582+maydeixxx@users.noreply.github.com>
2024-09-10 16:39:27 +0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,10 +5,10 @@ int main()
 {
 	float y, z;
 	printf("Input y:");
-	scanf_s("%f", &y);
+	scanf("%f", &y);
 
 	printf("Input z:");
-	scanf_s("%f", &z);
+	scanf("%f", &z);
 
 	float t = 5 / (1 + pow(y, 2));
 	float x = 2 * y + 3 * sinh(t) - z;
