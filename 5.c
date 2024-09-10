17a69ebc84ad510a0fc02a323a32d3ab79692d49
Alex Amaev <139319582+maydeixxx@users.noreply.github.com>
2024-09-10 16:40:07 +0700

    Update 5.c

--- 5.c
+++ 5.c
@@ -5,10 +5,10 @@ int main()
 {
 	float y, z;
 	printf("Input y:");
-	scanf_s("%f", &y);
+	scanf("%f", &y);
 
 	printf("Input z:");
-	scanf_s("%f", &z);
+	scanf("%f", &z);
 
 	float t = sin(2 + z);
 	float x = 6 * pow(t, 2) - ((cos(z) / sin(z)) + 1) / pow(y, 2);
