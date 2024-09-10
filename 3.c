346b885bdbe8fb05053b98f6d1172e7ea2f2987b
Alex Amaev <139319582+maydeixxx@users.noreply.github.com>
2024-09-10 16:39:49 +0700

    Update 3.c

--- 3.c
+++ 3.c
@@ -5,10 +5,10 @@ int main()
 {
 	float t, z;
 	printf("Input t:");
-	scanf_s("%f", &t);
+	scanf("%f", &t);
 
 	printf("Input z:");
-	scanf_s("%f", &z);
+	scanf("%f", &z);
 
 	float y = sin(t);
 	float x = 4 * pow(y, 2) / (4 * y * exp(z) - 2 * pow(t, 3));
